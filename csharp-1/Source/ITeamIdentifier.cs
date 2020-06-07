fe66ae787b9f6b5b682137fb60a5596dae8ba427
thiagovidigall <thiagovidigall@gmail.com>
2020-06-07 17:11:17 -0300

    Add aceleraDev C#

---
+++ csharp-1/Source/ITeamIdentifier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codenation.Challenge
+{
+    public interface ITeamIdentifier
+    {
+        long TeamId { get; set; }
+    }
+}
---
+++ csharp-7/Source/Models/Candidate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codenation.Challenge.Models
+{
+    public class Candidate
+    {
+
+        //chaves estrangeiras
+        public int User_Id { get; set; }
+        public int Acceleration_Id { get; set; }
+        public int Company_Id { get; set; }
+        //chaves estrangeiras
+
+        public int Status { get; set; }      
+        public DateTime Created_At { get; set; }
+
+
+        //propriedade de navegação
+        public User User { get; set; }
+
+        public Acceleration Acceleration { get; set; }
+        
+        public Company Company { get; set; }
+        //propriedade de navegação
+    }
+}
